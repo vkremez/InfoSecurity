4b8aca1a93853480aa2d7063b3d1c610abe2e86a
Vitali's Repo <vkremez@users.noreply.github.com>
2015-10-26 02:07:26 -0400

    Update WebSiteURLExtractor.py

--- WebSiteURLExtractor.py
+++ WebSiteURLExtractor.py
@@ -6,7 +6,7 @@ import requests
 m_url = raw_input("Enter a website to extract the URL's from: ")
 r_url = requests.get("http://" +url)
 
-data = r.text
+data = r_url.text
 soup = BeautifulSoup(data)
 
 for link in soup.find_all('a'):

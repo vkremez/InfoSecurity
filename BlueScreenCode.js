3a4032a1a17735cc7bf22371de2e0ee318fd048c
Vitali's Repo <vkremez@users.noreply.github.com>
2015-11-15 17:15:30 -0500

    Create BlueScreenCode.js

---
+++ BlueScreenCode.js
@@ -0,0 +1,18 @@
+// __author__ = vkremez
+
+image = new SimpleImage("___.jpg");
+back = new SimpleImage("____.jpg");
+back.setSameSize(image);
+
+for (pixel: image) {
+  avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+  avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+  if (pixel.getBlue() > avg * 1.1) {
+    x = pixel.getX();
+    y = pixel.getY();
+    pixel2 = back.getPixel(x, y);
+    pixel.setRed(pixel2.getRed());
+    pixel.setGreen(pixel2.getGreen());
+    pixel.setBlue(pixel2.getBlue());
+}
+print(image);
